3e03ea5000e84681b78f2e8187aa53da1d81a680
Aris <ariskak14@gmail.com>
2019-09-27 01:40:38 +0300

    Almost Finalized

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init middleware
 // app.use(logger)
+
+// Handlebars Middlware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 // Body Parser Middleware
 app.use(express.json());
@@ -13,8 +21,15 @@ app.use(express.urlencoded({
     extended: false
 }));
 
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Members API Routes
 app.use('/api/members', require('./routes/api/members'));
 
